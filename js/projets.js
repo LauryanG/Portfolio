9abe5753bbdedc5cd33ad71367338bf70daa77fa
Lauryan <lauryan-gervais@hotmail.com>
2021-03-08 12:20:45 -0500

    Page à propos terminer

--- js/projets.js
+++ js/projets.js
@@ -6,21 +6,38 @@ function afficherProjet() {
 
     for (let i = 0; i < lesProjets.length; i++) {
 
-        chaineHtml += "<div class=\"accordion-item\">";
-        chaineHtml += "<h2 class=\"accordion-header\" id=\"flush-headingOne\">";
-        chaineHtml += "<button class=\"accordion-button collapsed\" type=\"button\" data-mdb-toggle=\"collapse\" data-mdb-target=\"#flush-collapseOne\" aria-expanded=\"false\" aria-controls=\"flush-collapseOne\">";
-        chaineHtml += lesProjets[i].titre;
-        chaineHtml += "</button>"
-        chaineHtml += "</h2>"
-        chaineHtml += "<div id=\"flush-collapseOne\" class=\"accordion-collapse collapse\" aria-labelledby=\"flush-headingOne\" data-mdb-parent=\"#accordionFlushExample\>";
-        chaineHtml += "<div class=\"accordion-body\">";
-        chaineHtml += lesProjets[i].description;
-        chaineHtml += "</div>";
-        chaineHtml += "</div>";
-        chaineHtml += "</div>";
+        chaineHtml += "<li>";
+        chaineHtml += "<div class=\"unProjet\"> <div style=\"background-image: url("+ lesProjets[i].images[0] +");\"  class=\"titre\"> <h2>" + lesProjets[i].titre + " </h2> </div>";
+        chaineHtml += "<div class=\"description\">" + "<p>" + lesProjets[i].description + "</p>" + "</div> </div>";
+        chaineHtml += "<div class=\"image-projet\"><img src=\""+lesProjets[i].images[0]+"\" alt=\"\"></div>";
+        chaineHtml += "</li>";
     }
 
-    document.querySelector('div.accordion').innerHTML = chaineHtml;
+    document.querySelector('ul.listeProjets').innerHTML = chaineHtml;
 }
 
 afficherProjet();
+ouvrirDescription();
+
+window.addEventListener('resize', ouvrirDescription);
+
+function ouvrirDescription() {
+  if ($(window).width() < 768) {
+    console.log($(window).width());
+  
+    let acc = document.getElementsByClassName("titre");
+    let i;
+  
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("ouvert");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+  }
+}
