a3a7d89658b5365a5faee1d467850ac05477b944
Lauryan <lauryan-gervais@hotmail.com>
2021-03-08 20:09:25 -0500

    Page projets

--- js/projets.js
+++ js/projets.js
@@ -2,13 +2,13 @@ function afficherProjet() {
     let lesProjets = projets;
     let chaineHtml = "";
 
-    // Parcourir le tableau projets pour générer les projets
+    // Parcourir le tableau projets pour générer les projets et injecter le code HTML
 
     for (let i = 0; i < lesProjets.length; i++) {
 
         chaineHtml += "<li class=\"bg-image\">";
         chaineHtml += "<div class=\"unProjet\"> <div style=\"background-image: url("+ lesProjets[i].images[0] +");\"  class=\"titre\"> <h2>" + lesProjets[i].titre + " </h2> </div>";
-        chaineHtml += "<div class=\"description\">" + "<p>" + lesProjets[i].description + "</p>" + "</div> </div>";
+        chaineHtml += "<div class=\"description\">" + "<p> Projet :" + lesProjets[i].titre + "  <br>" + lesProjets[i].description + "</p>" + "</div> </div>";
         chaineHtml += "<div class=\"image-projet\"><img src=\""+lesProjets[i].images[0]+"\" alt=\"\"></div>";
         chaineHtml += "</li>";
     }
@@ -18,20 +18,21 @@ function afficherProjet() {
 
 afficherProjet();
 
+// Donne la classe droite à chaque li pair
 $( ".listeProjets li:even" ).addClass('droite');
 
-// Affiche les descriptions de projets
+// Toggle la classe ouvert des li lorsque cliquer
   let acc = document.getElementsByClassName("titre");
   let i;
 
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-      // this.classList.toggle("ouvert");
-      let panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    });
-  }
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("ouvert");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
